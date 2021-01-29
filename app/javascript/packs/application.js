52049eb1bed998b00a114524dbefa5872568c6c4
Masahiro Okubo <stmhamachiii@gmail.com>
2021-01-29 22:01:16 +0900

    bootstrapが入っていることを確認
    bootstrapが入っていることを確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ require('channels')
 
 
 import 'bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
